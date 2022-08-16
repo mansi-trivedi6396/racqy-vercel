abf1f74db42b15125b5252401ef1c6973e5ff906
mansi <mansitrivedi315@gmail.com>
2022-08-16 14:02:59 +0530

    edit footer

--- src/pages/Admin/ThemeFile/ButtonColor.js
+++ src/pages/Admin/ThemeFile/ButtonColor.js
@@ -1,1 +1,8 @@
-export const ButtonColor='#45C165'
+// export const ButtonColor='#45C165'
+import React from 'react'
+
+export default function ButtonColor() {
+  return (
+    <div>ButtonColor</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@
 // }
 import React from 'react'
 import Appbar from "./Admin/Component/Appbar"
-import Footer from './Admin/Component/Footer'
+import Footer from "./Admin/Component/Footer"
 
 export default function index() {
   return (
