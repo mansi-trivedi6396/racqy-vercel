f790f506685e4e1f6f14082c78ebdcc3dece059e
mansi <mansitrivedi315@gmail.com>
2022-08-16 13:49:32 +0530

    edit index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,32 @@
-import React from "react"
-import { Match } from '@reach/router'
+// import React from "react"
+// import { Match } from '@reach/router'
 
 
 
-export default function Home() {
+// export default function Home() {
   
-  <>
-    <Match path="/">
-      {
-        window.location.href="/Admin/Component/Appbar"
-      }
-    </Match>
+//   <>
+//     <Match path="/">
+//       {
+//         window.location.href="/Admin/Component/Appbar"
+//       }
+//     </Match>
 
   
    
     
-  </>
+//   </>
 
+// }
+import React from 'react'
+import Appbar from "./Admin/Component/Appbar"
+
+export default function index() {
+  return (
+    <>
+    <Appbar/>
+    
+   
+    </>
+  )
 }
